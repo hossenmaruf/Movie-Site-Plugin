d0618d03ec0518b7b6f68d5a541e101669278745
hossenmaruf <severusmaruf740@gmail.com>
2022-12-09 04:09:10 +0600

    first commit

---
+++ Movie_site_plugin.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @package  alecaddPlugin
+ */
+/*
+Plugin Name: move_site_plugin
+Plugin URI: https://github.com/hossenmaruf
+Description: This is my first attempt on writing a custom Plugin
+Version: 1.0.0
+Author: hossenmaruf
+Author URI: https://github.com/hossenmaruf
+License: GPLv2 or later
+Text Domain: alecadd-plugin
+*/
+
+
+// If this file is called firectly, abort!!!
+defined( 'ABSPATH' ) or die( 'Hey, what are you doing here? You silly human!' );
+
+// Require once the Composer Autoload
+if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
+	require_once dirname( __FILE__ ) . '/vendor/autoload.php';
+}
+
+/**
+ * The code that runs during plugin activation
+ */
+// function activate_alecadd_plugin() {
+// 	Inc\Base\Activate::activate();
+// }
+// register_activation_hook( __FILE__, 'activate_alecadd_plugin' );
+
+// /**
+//  * The code that runs during plugin deactivation
+//  */
+// function deactivate_alecadd_plugin() {
+// 	Inc\Base\Deactivate::deactivate();
+// }
+// register_deactivation_hook( __FILE__, 'deactivate_alecadd_plugin' );
+
+/**
+ * Initialize all the core classes of the plugin
+ */
+if ( class_exists( 'Inc\\Init' ) ) {
+	Inc\Init::register_services();
+}
+
+echo 'hello' ; 
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sever/movie_site_plugin',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '467c2b53d1763e77f113f93909f5f6f078a9b559',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sever/movie_site_plugin' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '467c2b53d1763e77f113f93909f5f6f078a9b559',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
